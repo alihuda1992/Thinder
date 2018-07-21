999225dd27fce47f30cca82b9378f62bebbc54b4
Ali Huda <alihuda1992@gmail.com>
2018-07-21 16:20:11 -0400

    updated server.js file

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // Static directory
-app.use(express.static("public"));
+app.use(express.static(path.join((__dirname, "thinder/build"))));
 
 // Routes
 // =============================================================
